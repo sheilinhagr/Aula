ab2a94dac48752356d982dd779b42556d4f007fe
Sheila Rezende <shegomesrez@gmail.com>
2026-02-24 12:54:31 -0300

    Update A48-INTRO-listas.py

--- A48-INTRO-listas.py
+++ A48-INTRO-listas.py
@@ -13,7 +13,7 @@ string = 'ABCDE'  # 5 caracteres (len)
 
 #        0    1      2              3    4
 #       -5   -4     -3             -2   -1
-lista = [123, True, 'Luiz Otávio',  1.2, []]
+lista = [123, True, 'Sheila',  1.2, []]
 lista[-3] = 'Maria'
 print(lista)
 print(lista[2], type(lista[2]))
